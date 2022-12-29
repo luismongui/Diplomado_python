a09b6b94f416483c6a793fc455bd25b3cc76eb63
Luis Eduardo Mongui Quevedo <luis.mongui@outlook.com>
2022-12-29 16:19:40 -0500

    practicas

---
+++ Capitlo 2/FunPrint.py
@@ -0,0 +1,26 @@
+print("mi primer programa")
+
+#Iniciando practicas
+
+#print("mi nombre es Luis")
+#print("practicas")
+#print('Practica comilla sencilla')
+#print('lista de inventario es: ')}
+print("*****************************")
+print("         Bienvenido          ")
+print("***********************")
+print('''
+      "** *********"
+      "** ********"
+      "**   **********"
+      "**       ******"
+      ''')
+print('''
+      
+      Feliz★* 。 • ˚ ˚ ˛ ˚ ˛ •
+•。★Navidad★ 。* 。
+° 。 ° ˛˚˛ * _Π_____*。*˚
+˚ ˛ •˛•˚ */______/~＼。˚ ˚ ˛
+˚ ˛ •˛• ˚ ｜ 田田 ｜門｜ ˚
+Un feliz año nuevo''')
+
---
+++ Capitlo 2/FunSep.py
@@ -0,0 +1,4 @@
+import os
+os.system("cls")
+
+print("funcion","sep","prueba de espacios",sep=" prueba espacio:")
